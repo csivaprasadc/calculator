57278bf78b5a46fe8ec70cde6833265eea54519b
Sivaprasad Chidambaram <csivaprasad.c@gmail.com>
2017-12-16 21:38:11 +0530

    Sum Logic and Unit Test Cases

--- src/test/java/com/prasad/tutorial/devops/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/prasad/tutorial/devops/calculator/CalculatorApplicationTests.java
@@ -2,15 +2,24 @@ package com.prasad.tutorial.devops.calculator;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalculatorApplicationTests {
 
+    @Autowired
+    private Calculator calculator;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testSum() {
+	    assertEquals(7,calculator.sum(3,4));
+    }
 }
