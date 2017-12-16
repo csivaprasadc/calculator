afeed666312ed388f2b728a66a1521cc8295ae92
Sivaprasad Chidambaram <csivaprasad.c@gmail.com>
2017-12-16 23:07:57 +0530

    Added Jacoco Coverage.

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,4 +6,13 @@ node {
     stage("Unit test") {
         sh "./gradlew test"
     }
+    stage("Code Coverage") {
+        sh "./gradlew jacocoTestReport"
+        publishHTML(target:[
+            reportDir: 'build/reports/jacoco/test/html',
+            reportFiles: 'index.html',
+            reportName: "JaCoCo Report"
+        ])
+        sh "./gradlew jacocoTestCoverageVerification"
+    }
 }
--- src/test/java/com/prasad/tutorial/devops/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/prasad/tutorial/devops/calculator/CalculatorApplicationTests.java
@@ -3,23 +3,47 @@ package com.prasad.tutorial.devops.calculator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class CalculatorApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
     private Calculator calculator;
 
+    @Autowired
+    private CalculatorController calculatorController;
+
 	@Test
 	public void contextLoads() {
+		assertThat(calculatorController).isNotNull();
 	}
 
 	@Test
 	public void testSum() {
 	    assertEquals(7,calculator.sum(3,4));
+    }
+
+    @Test
+    public void shouldReturnSum() throws Exception {
+	    this.mockMvc.perform(get("/sum").param("a","1").param("b","2"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("3")));
     }
 }
